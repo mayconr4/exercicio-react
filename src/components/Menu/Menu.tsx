d485509a99035e12c910fc72ab5e41079bd50ae1
mayconr4 <mayconhs2007@gmail.com>
2025-07-25 11:58:52 -0300

    feito menu

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,10 @@
+export default function Menu() {
+  return (
+    <nav>
+      <a href="">Home</a>
+      <a href="">Exemplo1</a>
+      <a href="">Exemplo2</a>
+      <a href="">Exemplo3</a>
+    </nav>
+  );
+}
